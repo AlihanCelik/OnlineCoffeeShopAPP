fd9e287e68f04807bee6cc3bcc73d96a1a0bbbde
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-09 01:25:15 +0300

    CoffeeApp

---
+++ app/src/main/java/com/example/onlincecoffeeshopapp/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.onlincecoffeeshopapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.onlincecoffeeshopapp.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.startBtn.setOnClickListener {
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
