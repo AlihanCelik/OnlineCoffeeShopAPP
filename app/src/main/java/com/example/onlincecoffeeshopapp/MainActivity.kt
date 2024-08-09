070d7a191250fdb6c41219ad8abaa7f53397b8a1
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-09 14:59:06 +0300

    CoffeeApp

--- app/src/main/java/com/example/onlincecoffeeshopapp/MainActivity.kt
+++ app/src/main/java/com/example/onlincecoffeeshopapp/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.onlincecoffeeshopapp.databinding.ActivityIntroBinding
+import com.example.onlincecoffeeshopapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController= Navigation.findNavController(this, R.id.host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationView,navController)
+
     }
 }
