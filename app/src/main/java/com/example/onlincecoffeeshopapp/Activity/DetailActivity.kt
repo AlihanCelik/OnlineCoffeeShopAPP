59e02ad13d5b092ad62fc78d0e7c3a1a12d67fc9
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-12 20:48:37 +0300

    CoffeeApp

--- app/src/main/java/com/example/onlincecoffeeshopapp/Activity/DetailActivity.kt
+++ app/src/main/java/com/example/onlincecoffeeshopapp/Activity/DetailActivity.kt
@@ -1,19 +1,12 @@
 package com.example.onlincecoffeeshopapp.Activity
 
-import android.content.ClipData.Item
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.Request
 import com.bumptech.glide.request.RequestOptions
 import com.example.onlincecoffeeshopapp.Adapter.SizeAdapter
 import com.example.onlincecoffeeshopapp.Model.ItemsModel
-import com.example.onlincecoffeeshopapp.R
 import com.example.onlincecoffeeshopapp.databinding.ActivityDetailBinding
 import com.example.project1762.Helper.ManagmentCart
 
